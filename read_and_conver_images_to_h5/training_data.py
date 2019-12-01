deef668f82b73ab6591b7f600d97ba323ff05140
Monjoy Saha <monjoybme@gmail.com>
2019-11-30 21:24:43 -0500

    New png to h5 conversion code 30Nov 2019 working
    New png to h5 conversion code 30Nov 2019 working code as previous code is running slow.

---
+++ read_and_conver_images_to_h5/training_data.py
@@ -0,0 +1,73 @@
+#http://machinelearninguru.com/deep_learning/data_preparation/hdf5/hdf5.html
+from random import shuffle
+import glob
+import numpy as np
+import h5py
+import cv2
+#import pdb
+#shuffle_data = False  # shuffle the addresses before saving
+hdf5_path = '/home/ms/Desktop/h5_code_30Nov/training_data.h5'  # address to where you want to save the hdf5 file
+cat_dog_train_path = '/home/ms/Desktop/h5_code_30Nov/training_data/*.png'
+# read addresses and labels from the 'train' folder
+addrs = glob.glob(cat_dog_train_path)
+#labels = [0 if 'cat' in addr else 1 for addr in addrs]  # 0 = Cat, 1 = Dog
+# to shuffle data
+#if shuffle_data:
+ #  c = list(zip(addrs))
+  # shuffle(c)
+   #addrs = zip(*c)
+        
+# Divide the hata into 60% train, 20% validation, and 20% test
+train_addrs = addrs[0:int(0.8*len(addrs))]
+#pdb.set_trace()
+##train_labels = labels[0:int(0.6*len(labels))]
+#val_addrs = addrs[int(0.6*len(addrs)):int(0.8*len(addrs))]
+##val_labels = labels[int(0.6*len(addrs)):int(0.8*len(addrs))]
+#test_addrs = addrs[int(0.8*len(addrs)):]
+##test_labels = labels[int(0.8*len(labels)):]
+
+#################################################################
+data_order = 'tf'  # 'th' for Theano, 'tf' for Tensorflow
+# check the order of data and chose proper data shape to save images
+if data_order == 'th':
+   train_shape = (len(train_addrs), 3, 224, 224)
+   #val_shape = (len(val_addrs), 3, 224, 224)
+   #test_shape = (len(test_addrs), 3, 224, 224)
+elif data_order == 'tf':
+   train_shape = (len(train_addrs), 224, 224, 3)
+   #val_shape = (len(val_addrs), 224, 224, 3)
+   #test_shape = (len(test_addrs), 224, 224, 3)
+# open a hdf5 file and create earrays
+hdf5_file = h5py.File(hdf5_path, mode='w')
+hdf5_file.create_dataset("train_img", train_shape, np.int8)
+#hdf5_file.create_dataset("val_img", val_shape, np.int8)
+#hdf5_file.create_dataset("test_img", test_shape, np.int8)
+#hdf5_file.create_dataset("train_mean", train_shape[1:], np.float32)
+#hdf5_file.create_dataset("train_labels", (len(train_addrs),), np.int8)
+#hdf5_file["train_labels"][...] = train_labels
+#hdf5_file.create_dataset("val_labels", (len(val_addrs),), np.int8)
+#hdf5_file["val_labels"][...] = val_labels
+#hdf5_file.create_dataset("test_labels", (len(test_addrs),), np.int8)
+#hdf5_file["test_labels"][...] = test_labels
+###################################################################
+# a numpy array to save the mean of the images
+mean = np.zeros(train_shape[1:], np.float32)
+# loop over train addresses
+for i in range(len(train_addrs)):
+    # print how many images are saved every 1000 images
+    if i % 1000 == 0 and i > 1:
+       print ('Train data: {}/{}'.format(i, len(train_addrs)))
+    # read an image and resize to (224, 224)
+    # cv2 load images as BGR, convert it to RGB
+    addr = train_addrs[i]
+    img = cv2.imread(addr)
+    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # add any image pre-processing here
+    # if the data order is Theano, axis orders should change
+    if data_order == 'th':
+       img = np.rollaxis(img, 2)
+    # save the image and calculate the mean so far
+    hdf5_file["train_img"][i, ...] = img[None]
+    #mean += img / float(len(train_labels))
+hdf5_file.close()
